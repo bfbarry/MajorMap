6ceed1b1da5f10545d7c84b0f46e0dd23fbc90a2
brianatUCSD <45830810+brianatUCSD@users.noreply.github.com>
2020-08-08 13:14:23 -0700

    housekeeping and documentation

--- utils/tools.py
+++ utils/tools.py
@@ -4,13 +4,19 @@ import urllib.request, urllib.error, urllib.parse
 from bs4 import BeautifulSoup
 
 def replace_all(unwanted, in_str):
+    """
+    Removes all instances of an unwanted string inside in_str
+    """
     for i in unwanted:
         in_str = in_str.replace(i, '')
     return in_str
 
- #this beautifulsoups those catalogs from the folder
 def extract_course_catalog(dept): #where dept is a string
-    f = open('utils/sites/' + dept + '_courses.html') #dept is all caps     
+    """
+    Extracts course names and descriptions (including prerequisites) from downloaded
+    course catalog page given department ID (string) using beautifulsoup
+    """
+    f = open('utils/sites/' + dept + '_courses.html') #dept is all caps
     soup = BeautifulSoup(f, 'html.parser')
     names = soup.find_all("p", {"class": "course-name"})
     descriptions = soup.find_all("p", {"class": "course-descriptions"}) #includes prereqs
